ea3cdbfe2a78a916eaa901c1f94d48420012ceb5
unknown <bernardocastro2006@gmail.com>
2023-08-05 12:15:36 -0300

    Inicio calculadora

---
+++ Projeto 1/main.py
@@ -0,0 +1,55 @@
+def somar(a, b):
+    return a + b
+
+def subtrair(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b == 0:
+        return "Erro: divisão por zero não é permitida!"
+    return a / b
+
+def resto(a, b):
+    return a % b
+
+def menu():
+    print("Escolha a operação:")
+    print("1 - Somar")
+    print("2 - Subtrair")
+    print("3 - Multiplicar")
+    print("4 - Dividir")
+    print("5 - Resto")
+    print("6 - Potência")
+    print("7 - ")
+    print("10 - Sair")
+
+while True:
+    menu()
+    opcao = input("Digite o número da operação desejada: ")
+
+    if opcao == "10":
+        print("Calculadora encerrada.")
+        break
+
+    if opcao not in ("1", "2", "3", "4"):
+        print("Opção inválida. Escolha uma das opções válidas.")
+        continue
+
+    numero1 = float(input("Digite o primeiro número: "))
+    numero2 = float(input("Digite o segundo número: "))
+
+    if opcao == "1":
+        print("Resultado: ", somar(numero1, numero2))
+    elif opcao == "2":
+        print("Resultado: ", subtrair(numero1, numero2))
+    elif opcao == "3":
+        print("Resultado: ", multiplicar(numero1, numero2))
+    elif opcao == "4":
+        print("Resultado: ", dividir(numero1, numero2))
+    elif opcao == "5":
+        print("Resultado: ", resto(numero1, numero2))
+    
+    print('\n')
